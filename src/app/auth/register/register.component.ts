7e7f62a9389afcabd36baea8df85274912c70557
HugoBustamante <zaske2008@hotmail.com>
2022-04-14 22:12:23 -0500

    Validaciones del registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -15,11 +15,13 @@ export class RegisterComponent implements OnInit {
   roles:any= ['admin','editor'];
 
   public registerForm= this.formBuilder.group({
-    nombre: ['Laura', [Validators.required]],
-    email: ['laura@gmail.com', [Validators.required, Validators.email]],
-    password: ['123456', [Validators.required]],
-    passwordValidate: ['123456', [Validators.required]],
+    nombre: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    passwordValidate: ['', [Validators.required]],
     role: ['', [Validators.required]],
+  }, {
+    validation: this.passwordsEquals('password', 'passwordValidate')//Llamamos el metodo acá que verifica que las contraseñas sean iguales para poderle pasar estos campos de password y passwordValidate.
   });
 
   constructor( private formBuilder:FormBuilder,
@@ -60,5 +62,44 @@ export class RegisterComponent implements OnInit {
   changeInputRole(event:any){//Resive un evento que en este caso será el elemento select que tiene los roles en el formulario
     this.getRoles?.setValue(event.target.value, {onlySelf:true});//Establecemos el valor que se escoge en el select input. La propiedad onlySelf es para mostrar el valor
   }
+
+  fieldsNoValidate(field:string):boolean{
+    //Tomamos todos los campos del formBuilder y si alguno es invalido y la bandera es false osea que el usuario no ha enviado datos en el formulario, pues este metodo nos va a devolver un true, por lo que nos servirá para indicar un error si el campo que le pasamos en el componente html está vacio, de lo contrario no se mostrara el error por lo que es un false:
+      if(this.registerForm.get(field)?.invalid && this.formBandera){
+        return true;
+      }else{
+        return false;
+      }
+  }
+
+  passwordNoValidate(){
+    const pass1= this.registerForm.get('password')?.value; //Conseguimos el valor digitado en el campo de password
+    const pass2= this.registerForm.get('passwordValidate')?.value; //Conseguimos el valor digitado en el campo de validación del password.
+
+    if((pass1 !== pass2) && this.formBandera){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  //Aplicamos el codigo para hacer la comparacion de los password para que sean:
+  //Este metodo lo llamaremos en el formBuilder donde estan los campos del formulario para pasarle los dos campos de password.
+  passwordsEquals(pass1:string , pass2:string){
+
+    return (formGroup: FormGroup) =>{
+      const pass1Control= formGroup.get(pass1);
+      const pass2Control= formGroup.get(pass2);
+
+      //Si las contraseñas ingresadas en lso campos son iguales:
+      if(pass1Control?.value === pass2Control?.value){
+        pass2Control?.setErrors(null);//Establece errores en este no establecemos ninguno porque las contraseñas son iguales
+
+      }else{//Si las constraseñas de los campos no son iguales:
+        pass2Control?.setErrors({noEsIgual:true});//Le establecemos un error
+      }
+    }
+  }
+
 
 }
